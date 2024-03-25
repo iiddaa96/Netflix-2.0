239c4821aabab9ea9f35d9476259a4124089a497
maryamakeyf <maryamosalad98@outlook.com>
2024-03-25 15:30:02 +0100

    add carousle for all images in landingpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,75 @@
+// "use client";
+// import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+// import { Carousel } from "react-responsive-carousel";
+// import "react-responsive-carousel/lib/styles/carousel.min.css";
+// import movies from "./Data/movies";
+
+// function TrendingMovies() {
+//   return (
+//     <div>
+//       <Typography variant="h4" gutterBottom>
+//         Trending Movies
+//       </Typography>
+//       <Carousel showThumbs={true} showStatus={false} infiniteLoop autoPlay>
+//         {movies.map((movie, index) => (
+//           <div key={index}>
+//             <Grid container justifyContent="center">
+//               <Grid item xs={12} sm={6} md={4} lg={3}>
+//                 <Card>
+//                   <CardMedia
+//                     component="img"
+//                     height="400"
+//                     image={movie.thumbnail}
+//                     alt={movie.title}
+//                   />
+//                   <CardContent>
+//                     <Typography variant="h6" gutterBottom>
+//                       {movie.title}
+//                     </Typography>
+//                   </CardContent>
+//                 </Card>
+//               </Grid>
+//             </Grid>
+//           </div>
+//         ))}
+//       </Carousel>
+//     </div>
+//   );
+// }
+
+// export default TrendingMovies;
 "use client";
-import { Card, CardContent, CardMedia, Grid, Typography } from "@mui/material";
-import { Carousel } from "react-responsive-carousel";
-import "react-responsive-carousel/lib/styles/carousel.min.css";
+
+import { Box } from "@mui/material";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick-theme.css";
+import "slick-carousel/slick/slick.css";
 import movies from "./Data/movies";
 
-function TrendingMovies() {
+const MovieCarousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 2,
+  };
+
   return (
-    <div>
-      <Typography variant="h4" gutterBottom>
-        Trending Movies
-      </Typography>
-      <Carousel showThumbs={true} showStatus={false} infiniteLoop autoPlay>
+    // Karusell för alla filmer
+    <Box>
+      <Slider {...settings}>
         {movies.map((movie, index) => (
           <div key={index}>
-            <Grid container justifyContent="center">
-              <Grid item xs={12} sm={6} md={4} lg={3}>
-                <Card>
-                  <CardMedia
-                    component="img"
-                    height="400"
-                    image={movie.thumbnail}
-                    alt={movie.title}
-                  />
-                  <CardContent>
-                    <Typography variant="h6" gutterBottom>
-                      {movie.title}
-                    </Typography>
-                  </CardContent>
-                </Card>
-              </Grid>
-            </Grid>
+            <img
+              src={movie.thumbnail}
+              style={{ maxWidth: "100%", height: "auto" }}
+            />
           </div>
         ))}
-      </Carousel>
-    </div>
+      </Slider>
+    </Box>
   );
-}
+};
 
-export default TrendingMovies;
+export default MovieCarousel;
