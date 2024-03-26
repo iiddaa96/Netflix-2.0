376f349bc9e16ffea257d9ef5c3464edee19d1ca
maryamakeyf <maryamosalad98@outlook.com>
2024-03-26 20:09:37 +0100

    add search icon from mui

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,60 +1,62 @@
 "use client";
 import MenuIcon from "@mui/icons-material/Menu";
-import { Button, IconButton, Menu } from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
+import { alpha, styled } from "@mui/material/styles";
 import Image from "next/image";
 import Link from "next/link";
-import * as React from "react";
 import LogoDesktop from "../images/logoDesktop.png";
 
+const Search = styled("div")(({ theme }) => ({
+  position: "relative",
+  borderRadius: theme.shape.borderRadius,
+  backgroundColor: alpha(theme.palette.common.white, 0.15),
+  "&:hover": {
+    backgroundColor: alpha(theme.palette.common.white, 0.25),
+  },
+  marginRight: theme.spacing(2),
+  marginLeft: 0,
+  width: "100%",
+  [theme.breakpoints.up("sm")]: {
+    marginLeft: theme.spacing(3),
+    width: "auto",
+  },
+}));
+
+const SearchIconWrapper = styled("div")(({ theme }) => ({
+  padding: theme.spacing(0, 2),
+  height: "100%",
+  position: "absolute",
+  pointerEvents: "none",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+}));
+
+const StyledInputBase = styled(InputBase)(({ theme }) => ({
+  color: "inherit",
+  "& .MuiInputBase-input": {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: {
+      width: "20ch",
+    },
+  },
+}));
+
 function ResponsiveAppBar() {
-  // Tillstånd för att hantera öppnande och stängning av navigeringsmenyn och användarmenyn
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
-  const [anchorElUser, setAnchorElUser] = React.useState<null | HTMLElement>(
-    null
-  );
-
-  // Funktioner för att öppna och stänga navigeringsmenyn och användarmenyn
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElUser(event.currentTarget);
-  };
-
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
-
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(null);
-  };
-
   return (
-    <AppBar
-      position="static"
-      component="header"
-      sx={{
-        backgroundColor: "#000",
-        boxShadow: "none",
-        color: "white",
-      }}
-    >
+    <AppBar position="static" component="header">
       <Container maxWidth="xl">
-        <Toolbar
-          disableGutters
-          sx={{
-            paddingY: "8px",
-            marginTop: "1rem",
-            paddingX: { xs: "10px", sm: "20px" },
-          }}
-        >
+        <Toolbar disableGutters>
           <Box
             component={Link}
             href="/"
@@ -69,15 +71,12 @@ function ResponsiveAppBar() {
           <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
             <IconButton
               size="large"
-              aria-label="account of current user"
-              aria-controls="menu-appbar"
-              aria-haspopup="true"
-              onClick={handleOpenNavMenu}
+              aria-label="menu"
+              sx={{ marginRight: 2 }}
               color="inherit"
             >
               <MenuIcon />
             </IconButton>
-            {/* Layout för mobil */}
             <Box
               component={Link}
               href="/"
@@ -85,7 +84,6 @@ function ResponsiveAppBar() {
                 textDecoration: "none",
               }}
             >
-              {/* logo namn mobil vy */}
               <Typography
                 variant="h6"
                 noWrap
@@ -102,70 +100,61 @@ function ResponsiveAppBar() {
                 <Image src={LogoDesktop} height={20} width={70} alt="Logo2" />
               </Typography>
             </Box>
-            <Menu
-              id="menu-appbar"
-              anchorEl={anchorElNav}
-              anchorOrigin={{
-                vertical: "bottom",
-                horizontal: "left",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "left",
-              }}
-              open={Boolean(anchorElNav)}
-              onClose={handleCloseNavMenu}
+          </Box>
+          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
+            <Box
               sx={{
-                display: { xs: "block", md: "none" },
+                display: "flex",
+                justifyContent: "space-around",
+                width: "100%",
               }}
             >
-              <Button component={Link} href="/Favorite" color="inherit">
-                Favoriter
-              </Button>
-              <Button component={Link} href="/" color="inherit">
-                Trending
-              </Button>
-              <Button component={Link} href="/" color="inherit">
-                Contact
-              </Button>
-            </Menu>
+              <Link href="/" passHref>
+                <Typography
+                  variant="button"
+                  component="a"
+                  sx={{
+                    color: "white",
+                    textDecoration: "none",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Home
+                </Typography>
+              </Link>
+              <Link href="/favorite" passHref>
+                <Typography
+                  variant="button"
+                  component="a"
+                  sx={{
+                    color: "white",
+                    textDecoration: "none",
+                    marginRight: "1rem",
+                  }}
+                >
+                  Favorites
+                </Typography>
+              </Link>
+              <Link href="/contact" passHref>
+                <Typography
+                  variant="button"
+                  component="a"
+                  sx={{ color: "white", textDecoration: "none" }}
+                >
+                  Contact
+                </Typography>
+              </Link>
+            </Box>
           </Box>
-
-          {/* Tillfälliga länkar till andra sidor desktop */}
-          <Box sx={{ flexGrow: 1, display: { xs: "none", md: "flex" } }}>
-            <Button component={Link} href="/Favorite" color="inherit">
-              Favorites
-            </Button>
-            <Button component={Link} href="/" color="inherit">
-              Trending
-            </Button>
-            <Button component={Link} href="/" color="inherit">
-              Contact
-            </Button>
-            <Button component={Link} href="/" color="inherit">
-              Home
-            </Button>
-          </Box>
-
-          <Box sx={{ flexGrow: 0 }}>
-            <Menu
-              sx={{ mt: "45px" }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            ></Menu>
-          </Box>
+          <Search>
+            <SearchIconWrapper>
+              <SearchIcon />
+            </SearchIconWrapper>
+            <StyledInputBase
+              placeholder="Search…"
+              inputProps={{ "aria-label": "search" }}
+            />
+          </Search>
         </Toolbar>
       </Container>
     </AppBar>
