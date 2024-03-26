f63112953c0bebecf0f0552c65a63dedf5a7ef2a
Ida Casperson <ida_casperson@hotmail.com>
2024-03-26 15:56:00 +0100

    Added Trending component in page MovieList

--- app/page.tsx
+++ app/page.tsx
@@ -14,6 +14,7 @@ import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 import movies from "./Data/movies";
 import FilmView, { Movie } from "./components/Film-view";
+import Trending from "./components/Trending";
 
 function MovieList() {
   const [selectedMovie, setSelectedMovie] = useState<Movie | null>(null);
@@ -98,7 +99,7 @@ function MovieList() {
           allowFullScreen
         ></iframe>
       </div>
-
+      <Trending />
       {/* Movie Carousel */}
       <div>
         <h2 style={{ color: "white", paddingLeft: "20px" }}>All movies</h2>
